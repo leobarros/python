1d1a25c597dbb1d11f71e41c8ba80c5067fcf5c3
Leonardo <nickolback@gmail.com>
2020-04-04 20:15:17 -0300

    Converting to PEP8

--- json/json_file.py
+++ json/json_file.py
@@ -1,26 +1,31 @@
-import urllib.request, json
+import urllib.request
+import json
+
 
 def read(url):
     with urllib.request.urlopen(url) as entrada:
         data = json.loads(entrada.read().decode())
-        print(data.get('manufacturer') + ': ' + '(' + data.get('model') + ': ' + data.get('version') + ')')
+        print(data.get('manufacturer') + ': ' +
+              '(' + data.get('model') + ': ' + data.get('version') + ')')
 
-        
+
 if __name__ == '__main__':
 
     def sony():
         read('http://tv.globoplay.com.br/native/Sony/Unicorn-2019/' +
              'apps/globoplay/versiondata.json')
-    
-    def iora():
-        read('http://tv.globoplay.com.br/native/Panasonic/Iora-2019/apps/globoplay/versiondata.json')
-    
-    def iris():
-        read('http://tv.globoplay.com.br/native/Panasonic/Iris-2019/apps/globoplay/versiondata.json')
-    
-    def tcl():
-        read('http://tv.globoplay.com.br/native/TCL/S4900/apps/globoplay/versiondata.json')
 
+    def iora():
+        read('http://tv.globoplay.com.br/native/Panasonic/Iora-2019/' +
+             'apps/globoplay/versiondata.json')
+
+    def iris():
+        read('http://tv.globoplay.com.br/native/Panasonic/Iris-2019/' +
+             'apps/globoplay/versiondata.json')
+
+    def tcl():
+        read('http://tv.globoplay.com.br/native/TCL/S4900/' +
+             'apps/globoplay/versiondata.json')
 
 iora()
 iris()
