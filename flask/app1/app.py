e5e416dce321f59128bc0251cd15dc64b8d9b7c5
Leonardo Barros <nickolback@gmail.com>
2022-07-11 08:18:20 -0300

    creating a list

--- flask/app1/app.py
+++ flask/app1/app.py
@@ -1,17 +1,32 @@
-from flask import Flask
+import db
+from flask import Flask, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome'
+    html = ['<ul>']
+    for username, user in db.users.items():
+        html.append(
+            f"<li><a href='/user/{username}'>{user['name']}</a></li>"
+        )
+        html.append('</ul>')
+        return '\n'.join(html)
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
 
-@app.route('/hi')
-def hi():
-    return 'Hi!'
+def profile(username):
+    user = db.users.get(username)
 
-app.run()
+    if user:
+        return f"""
+            <h1>{user['name']}</h1>
+            <img src="{user['image']}"/><br/>
+            telefone: {user['tel']} <br/>
+            <a href="/">Voltar</a>
+        """
+    else:
+        return abort(404, "User not found")
+
+app.add_url_rule('/user/<username>/', view_func=profile, endpoint='user')
+        
+app.run(use_reloader=True)
---
+++ flask/app1/db.py
@@ -0,0 +1,17 @@
+users = {
+  "donald": {
+    "name": "Pato Donald",
+    "image": "http://1.bp.blogspot.com/-EIdGFOlOGTE/VWepQM9ehEI/AAAAAAAA2hE/vhQ0Z1xiIk8/s1600/clipdonhead.png",
+    "tel": "2114578541"
+    },
+  "mickey": {
+    "name": "Mickey Mouse",
+    "image": "https://2.bp.blogspot.com/-FKgkmE-rCME/VWepT2qhETI/AAAAAAAA2fU/257SoB_v9Ho/s320/mickey_smile.png",
+    "tel": "21145747555"
+    },
+  "marie": {
+    "name": "Cat Marie",
+    "image": "https://vignette.wikia.nocookie.net/disney/images/5/57/MarieCat.png/revision/latest?cb=20170506191611&path-prefix=pt-br",
+    "tel": "2145678999"
+    }
+}
